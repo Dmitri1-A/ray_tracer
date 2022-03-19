91686eda78621b5453bc607e71c100db491e586e
Anistratov Dmitry <anistratovd88@gmail.com>
2022-03-20 01:24:03 +0300

    material metal

--- hittable.h
+++ hittable.h
@@ -3,14 +3,19 @@
 #define HITTABLE_H
 
 #include "ray.h"
+#include "rtweekend.h"
 
-struct hit_record {
+class material;
+
+class hit_record {
+public:
     point3 p;
     vec3 normal;
+    shared_ptr<material> mat_ptr;
     double t;
     bool front_face;
 
-    inline void set_face_normal(const ray& r, const vec3& outward_normal) {
+    void set_face_normal(const ray& r, const vec3& outward_normal) {
         front_face = dot(r.direction(), outward_normal) < 0;
         normal = front_face ? outward_normal : -outward_normal;
     }
